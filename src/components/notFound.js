90e907a41cda0a92bac477dc530d665befa1ef3b
Thoufeeq Ahamed Jaffar Sadiq <jsthoufeeq@gmail.com>
2019-12-12 09:52:17 +0530

    Added notfound component

---
+++ src/components/notFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFound = () => {
+  return (
+    <div className="col text-center">
+      <p>Page not found. Please specify a valid URL</p>
+    </div>
+  )
+}
+
+export default notFound;
